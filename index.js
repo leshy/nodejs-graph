66a78dc66bdbfaa8527940f1032ba8ebcdc98d9f
lesh <lesh@sysphere.org>
2012-07-08 16:24:16 -0400

    ..

--- index.js
+++ index.js
@@ -77,11 +77,11 @@ var GenericGraphNode = Backbone.Model.extend({
             if (!self.plugs[plugname]) { throw "graph node can't find plug named '" + plugname + "'"; return; }
             return f(args);
         };
-        
+        /*
         _.map(['get', 'add', 'remove', 'has'], function(fname) {
             self.fname = decorate(plugfundecorator,self.fname);
         });
-
+        */
         this.plugs = {};
     },
 
@@ -107,8 +107,8 @@ var GenericGraphNode = Backbone.Model.extend({
         this[ 'get' + plugsingular ] = function() { return _.first(self.plugget.call(self,plugplural)); };
         this[ 'get' + plugplural ] = function() { return self.plugget.call(self,plugplural); };
         this[ 'replace' + plugsingular ] = function(obj1,obj2) { return self.plugreplace.apply(self,[plugplural,obj1,obj2]); };
-        this[ 'on' + plugsingular ] = function(callback) { return self.plugon.call(self,plugplural,callback) }
 
+        this[ 'on' + plugsingular ] = function(callback) { return self.plugon.call(self,plugplural,callback) }
         // you can preinitialize contents of the plug
         var toadd = (this.get(plugplural) || [])
         this.unset(plugplural)
@@ -120,12 +120,12 @@ var GenericGraphNode = Backbone.Model.extend({
         _.map(toadd, function(el) { add(el) })
     },
 
-    remove: function() {
+    del: function() {
         _.map(this.plugs,(function(plug,plugname) {
             this.plugremoveall(plugname)
         }.bind(this)))
 
-        this.trigger('remove')
+        this.trigger('del')
     },
 
     plugget: function(plug) {
@@ -169,23 +169,8 @@ var GenericGraphNode = Backbone.Model.extend({
     }
 });
 
-var GraphNode = GenericGraphNode.extend4000({
-    initialize: function() {
-        var self = this;
-        this.addplug('connections','connection');
-        this.connections.on('add',function(obj) { obj.addconnection(self); });
-        this.connections.on('remove',function(obj) { obj.delconnection(self); });
-        // in case the object was prefilled
-        this.connections.map(function(obj) { obj.addconnection(self) });
-
-        this.on('destroy',this.delconnections)
-    },
-
-    destroy: function () { this.trigger('destroy') }    
-})
-
 // GraphNode specializes GenericGraphNode by adding 'children' and 'parents' plugs
-var DirectedGraphNode = GenericGraphNode.extend4000({
+var GraphNode = GenericGraphNode.extend4000({
     initialize: function() {
         var self = this;
 
@@ -196,20 +181,19 @@ var DirectedGraphNode = GenericGraphNode.extend4000({
             obj.addchild(self);
         });
 
-        this.parents.on('remove',function(obj) { obj.delchild(self); });
-
-        this.children.on('add',function(obj) { obj.addparent(self); });
-
-        this.children.on('remove',function(obj) { obj.delparent(self); });
+        this.children.on('add',function(obj) {
+            obj.addparent(self);
+        });
+        
 
         // in case the object was prefilled
         this.children.map(function(child) {
             child.addparent(self)
-        });
+        })
 
         this.parents.map(function(child) {
             child.addchild(self)
-        });
+        })
 
     },
 
@@ -258,6 +242,5 @@ var TraversalMixin = Backbone.Model.extend4000({
 
 
 exports.GenericGraphNode = GenericGraphNode
-exports.DirectedGraphNode = DirectedGraphNode
 exports.GraphNode = GraphNode
 exports.TraversalMixin = TraversalMixin
