76ae9209220fa11219dc912248c017e7d2777a8f
lesh <lesh@sysphere.org>
2012-07-30 13:25:07 -0400

    package.json

--- index.js
+++ index.js
@@ -1,66 +1,7 @@
-var Backbone = require('backbone');
+var Backbone = require('backbone4000');
 var _ = require('underscore');
 var decorators = require('decorators');
 var decorate = decorators.decorate;
-
-// defaults attribute inheritance, and automatic super.initialize calls
-(function () {
-    function extend4000 () {
-        var args = Array.prototype.slice.call(arguments),
-        child = this;
-
-        var initf = [];
-        var defaults = {};
-        if (child.prototype.defaults) {
-            defaults = _.clone(child.prototype.defaults);
-        }
-
-        _.each(args, function (superc) {
-            // did I receive a dictionary or an object/backbone model?
-            if (superc.prototype) { superc = superc.prototype; }
-
-            // inherit defaults
-            if (superc.defaults) {
-                defaults = _.extend(defaults,superc.defaults);
-            }
-
-            // build a list of initialize functions if you find more then one
-            if (superc.initialize) {
-                (initf.length) || initf.push(child.prototype.initialize);
-                initf.push(superc.initialize);
-            }
-
-            child = child.extend(superc);
-        });
-
-        // construct a combined init function
-        if (initf.length) {
-            child = child.extend({ initialize : function(attributes,options) {
-                var self = this;
-                _.map(initf,function(initf) { initf.call(self,attributes,options); });
-            }});
-        }
-        child.prototype.defaults = defaults;
-        return child;
-    }
-
-    Backbone.Model.extend4000 =
-    Backbone.Collection.extend4000 =
-    Backbone.Router.extendEach =
-    Backbone.View.extend4000 = extend4000;
-
-    function triggerOnce(event,f) {
-        var self = this;
-        this.bind(event,function() {
-            self.unbind(event,f);
-            f.apply(this,toArray(arguments));
-        });
-    }
-
-    Backbone.Model.triggerOnce = triggerOnce;
-
-})();
-
 
 // a node that connects to other nodes via 'plugs'
 // plug is just a name of collection that contains other nodes
